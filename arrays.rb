25c1876f359012b5049953757d34044e2e6a9e1f
Mike Dacanay <migd64@gmail.com>
2017-10-02 16:44:03 -0700

    figuring the basic array build in methods out

---
+++ arrays.rb
@@ -0,0 +1,22 @@
+b = [1,2,3,5,45,3,363,4,32432]
+c = ["barracade", "magic", "fire", "wow", "elements"]
+
+
+# puts b.at(3);puts c.at(2);
+
+# b.delete(3)
+# print b;
+# c.delete("barracade")
+# print c;
+
+# d = c.reverse
+# print c
+# print d
+
+# puts b.length
+
+# puts c.sort {|a, b| b <=> a}
+
+e = b.slice!(2)
+puts e
+print b
